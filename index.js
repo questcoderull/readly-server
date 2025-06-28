11e11750a1cb91fd812f3b94213435d85b5e9b49
questcoderull <questcoderull@gmail.com>
2025-06-28 06:43:36 +0600

    added get api for commetnsCollection

--- index.js
+++ index.js
@@ -65,6 +65,11 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/comments", async (req, res) => {
+      const result = await commentsCollection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
